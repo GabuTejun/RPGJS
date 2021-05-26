90d2f275de9bc63c44cc8eb5e99978b7af492522
Gabriel Passos <gabutejun@gmail.com>
2021-05-26 15:44:26 -0300

    newAtt2

--- script.js
+++ script.js
@@ -12,19 +12,47 @@ let player = {
 };
 let enemys = [{
     nameMonster: "Slime",
-    minStats: 2,
-    maxStats: 5,
+    minStats: 1,
+    maxStats: 2,
+},{
+    nameMonster: "Zombie",
+    minStats: 3,
+    maxStats: 6,
 }];
 player.vit = player.vitBase
 player.str = player.strBase
 player.dex = player.dexBase
 playerStats("hidden")
+console.log(enemys)
+
+
+function loadPlayer() {
+    const playerStatsStorage = localStorage.getItem('player');
+    player = JSON.parse(playerStatsStorage)
+    playerStats("hidden")
+}
+
+function loadMonster() {
+    const monsterStorage = localStorage.getItem('enemys');
+    enemys = JSON.parse(monsterStorage)
+}
+
+function savePlayer() {
+    const playerJSON = JSON.stringify(player)
+    localStorage.setItem('player', playerJSON)
+};
+
+function saveMonster() {
+    const monsterJSON = JSON.stringify(enemys)
+    localStorage.setItem('enemys', monsterJSON)
+}
 
 function createMonster() {
     document.getElementById('nameMonster').hidden = false;
     document.getElementById('minStats').hidden = false;
     document.getElementById('maxStats').hidden = false;
     document.getElementById('sendMonster').hidden = false;
+    document.getElementById('createMonster').disabled = true;
 }
 
 function verifyNewMonster() {
@@ -56,6 +84,8 @@ function newMonster(nameMonster, minStats, maxStats) {
         document.getElementById('minStats').hidden = true;
         document.getElementById('maxStats').hidden = true;
         document.getElementById('sendMonster').hidden = true;
+        document.getElementById('createMonster').disabled = false;
+        saveMonster()
     } else {
         document.getElementById('response').innerHTML = "Impóssivel criar um monstro assim! Por Favor altere alguma informação para continuar."
     }
@@ -68,6 +98,7 @@ function spawnMonster() {
     dexMonster = Number(Math.round(Math.random() * (enemys[enemy].maxStats - enemys[enemy].minStats) + enemys[enemy].minStats - enemys[enemy].minStats))
     expMonster = Number(Math.round((strMonster + vitMonster + dexMonster)/4))
     document.getElementById('enemy').innerHTML = "Monstro : " + enemys[enemy].nameMonster + "<br>Força = " + strMonster + "<br>Defesa = " + dexMonster + "<br>Vida = " + vitMonster
+    document.getElementById('battle').disabled = false;
 }
 
 function battle() {
@@ -84,9 +115,9 @@ function battle() {
     if (vitMonster <= 0){
         player.expPlayer += expMonster
         player.vit = player.vitBase
-        spawnMonster()
+        document.getElementById('battle').disabled = true
         playerStats("hidden")
-
+        document.getElementById('enemy').innerHTML = ""
         if (player.expPlayer >= player.expForLevelUp){
             player.levelPlayer += 1
             player.expPlayer = 0
@@ -100,18 +131,35 @@ function battle() {
     }
 
     if (player.vit <= 0){
-
+        if (player.expPlayer > 0){
+            player.expPlayer = Math.floor(player.expPlayer/2)
+        } else {
+            if (player.levelPlayer > 1){
+            statsLose = Math.floor(player.levelPlayer/2)
+            console.log(statsLose)
+            player.strBase = player.strBase -statsLose
+            player.vitBase = player.vitBase -statsLose
+            player.dexBase = player.dexBase -statsLose
+            player.vit = player.vitBase
+            player.str = player.strBase
+            player.dex = player.dexBase
+            player.levelPlayer += -1
+            player.expForLevelUp = Math.round(player.expForLevelUp/1.5)
+        }}
+        player.vit = player.vitBase
+            document.getElementById('enemy').innerHTML = ""
+            document.getElementById('battle').disabled = true
+            playerStats("hidden")
     }
-
-    console.log(player.expPlayer, player.expForLevelUp, player.levelPlayer)
-    console.log(player.statsUp)
 }
 
 
 function playerStats(hidden) {
+    document.getElementById('level').innerHTML = "Level = " + player.levelPlayer
     document.getElementById('strPlayer').innerHTML = "Força = " + player.str + "<button id='strUp' " + hidden + " onclick='statsUp(`strBase`)'>+</button>"
     document.getElementById('dexPlayer').innerHTML = "Defesa = " + player.dex + "<button id='dexUp' " + hidden + " onclick='statsUp(`dexBase`)'>+</button>"
     document.getElementById('vitPlayer').innerHTML = "Vida = " + player.vit + "<button id='vitUp' " + hidden + " onclick='statsUp(`vitBase`)'>+</button>"
+    document.getElementById('exp').innerHTML = "Exp = " + player.expPlayer + "/" + player.expForLevelUp
 }
 
 function statsUp(stat) {
@@ -125,6 +173,5 @@ function statsUp(stat) {
     if (player.statsUp == 0) {
         playerStats("hidden")
         document.getElementById('spawnMonster').disabled = false
-        document.getElementById('battle').disabled = false
     }
 }
